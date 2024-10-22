238bd5df7ede4e06969572ef45b13a899bacc1d4
Aliaksei Dziardziyashenka <aleksejdior@gmail.com>
2024-10-22 22:56:51 +0300

    Add new files in my HW3

--- MentoringHomeWork/HW2/HW2.1/HW2.1.cs
+++ MentoringHomeWork/HW2/HW2.1/HW2.1.cs
@@ -1,14 +1,20 @@
+using System;
+
 namespace MentoringHomeWork.HW2.HW2._1;
 
-class Program
+public class Program1
 {
-    void Main(string[] args)
+   public void Main(string[] args)
     {
+         
+        Console.WriteLine("Простой калькулятор");
         double number = 5;
         var number22 = (int) number;
         number = number22;
         object a = number;
         object b = (object) number;
         int number3 = (int) number; 
+        Console.WriteLine("sdsadasdad");
+
     }
 }
