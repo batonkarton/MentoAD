678086b207945c48a2c22f38a78e132ed9a0007c
Aliaksei Dziardziyashenka <aleksejdior@gmail.com>
2024-11-04 17:31:29 +0300

    no message

--- MentoringHomeWork/HW3.0/HW03.Birthday/Program.cs
+++ MentoringHomeWork/HW3.0/HW03.Birthday/Program.cs
@@ -1,5 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
+<<<<<<< HEAD
 using HW03.Birthday;
 
 var birthdayCalculator = new BirthdayCalculator();
 birthdayCalculator.CalculatePersonAge();
+=======
+
+namespace HW03.Birthday;
+
+class Birthday
+{
+    private int month;
+    private int year;
+    private int monthCurrent;
+    private int yearCurrent;
+    public void Run()
+    {
+        BirthYear();
+        CurrentYear();
+        AgeDetermination();
+    }
+    void BirthYear()
+    {
+        Console.WriteLine("Введите месяц своего рождения:");
+        month =Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите год своего рождения:");
+        year = Convert.ToInt32(Console.ReadLine());
+    }
+    private void CurrentYear()
+    {
+        Console.WriteLine("Введите месяц текущего года:");
+        monthCurrent =Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Какой сейчас год?:");
+        yearCurrent = Convert.ToInt32(Console.ReadLine());
+    }
+
+    private void AgeDetermination()
+    {
+        int resultMonth = monthCurrent - month;
+        int resultYear = yearCurrent - year;
+        if (resultMonth < 0)
+        {
+            resultYear--;
+        }
+        Console.WriteLine("Ваш возраст:" /*+ resultMonth + " месяцев и */ + resultYear + " лет");
+    }
+    static void Main(string[] args)
+    {
+        Birthday birthday = new Birthday();
+        birthday.Run();
+    }
+}
+>>>>>>> 63e10f4 (HW3 birthday)
