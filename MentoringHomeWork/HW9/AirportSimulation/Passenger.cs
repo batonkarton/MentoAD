29a6ce5907553e4a37c240f84dcea4cf566b9acd
Aliaksei Dziardziyashenka <aleksejdior@gmail.com>
2025-03-04 11:20:36 +0300

    MT-25 added review fixes 3

--- MentoringHomeWork/HW9/AirportSimulation/Passenger.cs
+++ MentoringHomeWork/HW9/AirportSimulation/Passenger.cs
@@ -4,6 +4,5 @@ public class Passenger(string firstName, string lastName, DateTime dateOfBirth, 
 {
     public string FirstName { get; } = firstName ?? throw new ArgumentNullException(nameof(firstName));
     public string LastName { get; } = lastName ?? throw new ArgumentNullException(nameof(lastName));
-    public DateTime DateOfBirth { get; } = dateOfBirth;
     public CountryPassengers Country { get; } = country;
 }
